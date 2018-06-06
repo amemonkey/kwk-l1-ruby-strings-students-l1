b3356e788f8364c1d5d421954bc6a120d73d5d77
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 21:58:24 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,10 +1,9 @@
-
+def invitation_party
 guest_name = "OG"
 party_name = "Pretty Cool Beach Party"
 date = "July 17"
 time = "3pm"
 host_name = "Your Best Friends, the Backyardigans"
-def invitation_party
   puts "Dear #{guest_name}, you are cordially invited to #{party_name} on #{date} at #{time}. Sincerely, #{host_name}"
 end
 
